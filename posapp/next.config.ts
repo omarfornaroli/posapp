bbea3f95b795d1a611b4e101aa6bdd66d3362272
Omar Fornaroli <OmarFornaroli@gmail.com>
2025-07-07 03:57:11 +0000

    Checking validity of types  .Failed to compile.
    ./next.config.ts:54:29

---
+++ posapp/next.config.ts
@@ -0,0 +1,54 @@
+
+import type {NextConfig} from 'next';
+import createNextIntlPlugin from 'next-intl/plugin';
+import withPWA from 'next-pwa';
+import runtime from 'next-pwa/cache';
+
+const withNextIntl = createNextIntlPlugin('./src/i18n.ts');
+
+const pwaConfig = {
+    dest: 'public',
+    register: true,
+    skipWaiting: true,
+    disable: process.env.NODE_ENV === 'development',
+    runtimeCaching: [
+      {
+        urlPattern: /(\/login|\/setup-account|\/reset-password|\/reports)/,
+        handler: 'NetworkOnly'
+      },
+      ...runtime,
+    ],
+};
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: false,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'flagcdn.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  serverExternalPackages: [
+    '@opentelemetry/sdk-node', 
+    'handlebars'
+  ],
+};
+
+const configWithPWA = withPWA(pwaConfig);
+export default configWithPWA(withNextIntl(nextConfig));
