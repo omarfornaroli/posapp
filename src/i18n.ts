197291783e346950e2c6a3c92e628f7c5c2c8af6
Omar Fornaroli <OmarFornaroli@gmail.com>
2025-07-18 22:41:32 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,21 +1,16 @@
 
-import {getLocale} from 'next-intl/server';
 import {notFound} from 'next/navigation';
 import {getRequestConfig} from 'next-intl/server';
 import {locales} from '@/i18n-config';
 
-export default getRequestConfig(async () => {
+export default getRequestConfig(async ({locale}) => {
   // This logic is executed for every request that starts with a locale.
   // The locale is received from the middleware.
-  const locale = await getLocale();
 
   // Validate that the incoming `locale` parameter is valid
   if (!locales.includes(locale as any)) notFound();
 
   return {
-    locale,
     messages: (await import(`@/messages/${locale}.json`)).default
   };
 });
-
-    
