5ae47f9bf586d76d846c2f01396b32fdb9d11897
Omar Fornaroli <OmarFornaroli@gmail.com>
2025-07-23 22:26:45 +0000

    src/components/ui/calendar.tsx:58:9 - error TS2353: Object literal may o

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -55,6 +55,7 @@ function Calendar({
         ...classNames,
       }}
       components={{
+        IconLeft: () => <ChevronLeft className="h-4 w-4" />,
         IconRight: () => <ChevronRight className="h-4 w-4" />,
       }}
       {...props}
