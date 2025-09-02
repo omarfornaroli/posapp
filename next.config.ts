a3fd6a67707e091cb2ff1757cb8fb220d7844fcf
Omar Fornaroli <OmarFornaroli@gmail.com>
2025-09-02 00:42:52 +0000

    sync.service.ts:143  POST https://6000-firebase-studio-1749948194212.cl

--- next.config.ts
+++ next.config.ts
@@ -11,10 +11,94 @@ const pwaConfig = {
     buildExcludes: [/app-build-manifest\.json$/], // Exclude this file from precaching
     runtimeCaching: [
       {
-        urlPattern: /(\/login|\/setup-account|\/reset-password|\/reports)/,
+        urlPattern: /^https:\/\/fonts\.(?:googleapis|gstatic)\.com\/.*/i,
+        handler: 'CacheFirst',
+        options: {
+          cacheName: 'google-fonts',
+          expiration: {
+            maxEntries: 4,
+            maxAgeSeconds: 365 * 24 * 60 * 60 // 365 days
+          }
+        }
+      },
+      {
+        urlPattern: /\.(?:eot|otf|ttc|ttf|woff|woff2|font.css)$/i,
+        handler: 'StaleWhileRevalidate',
+        options: {
+          cacheName: 'static-font-assets',
+          expiration: {
+            maxEntries: 4,
+            maxAgeSeconds: 7 * 24 * 60 * 60 // 7 days
+          }
+        }
+      },
+      {
+        urlPattern: /\.(?:jpg|jpeg|gif|png|svg|ico|webp)$/i,
+        handler: 'StaleWhileRevalidate',
+        options: {
+          cacheName: 'static-image-assets',
+          expiration: {
+            maxEntries: 64,
+            maxAgeSeconds: 24 * 60 * 60 // 24 hours
+          }
+        }
+      },
+      {
+        urlPattern: /\.(?:js)$/i,
+        handler: 'StaleWhileRevalidate',
+        options: {
+          cacheName: 'static-js-assets',
+          expiration: {
+            maxEntries: 32,
+            maxAgeSeconds: 24 * 60 * 60 // 24 hours
+          }
+        }
+      },
+      {
+        urlPattern: /\.(?:css|less)$/i,
+        handler: 'StaleWhileRevalidate',
+        options: {
+          cacheName: 'static-style-assets',
+          expiration: {
+            maxEntries: 32,
+            maxAgeSeconds: 24 * 60 * 60 // 24 hours
+          }
+        }
+      },
+      {
+        urlPattern: /\.(?:json|xml|csv)$/i,
+        handler: 'NetworkFirst',
+        options: {
+          cacheName: 'static-data-assets',
+          expiration: {
+            maxEntries: 32,
+            maxAgeSeconds: 24 * 60 * 60 // 24 hours
+          }
+        }
+      },
+      {
+        urlPattern: /(\/login|\/setup-account|\/reset-password)/,
         handler: 'NetworkOnly'
       },
-      ...runtime,
+       {
+        urlPattern: ({ request, url }) => {
+          if (request.mode !== 'navigate') {
+            return false;
+          }
+          if (url.pathname.startsWith('/api')) {
+            return false;
+          }
+          return true;
+        },
+        handler: 'StaleWhileRevalidate',
+        options: {
+          cacheName: 'pages-cache',
+          expiration: {
+              maxEntries: 64,
+              maxAgeSeconds: 24 * 60 * 60 // 24 hours
+          }
+        }
+      }
     ],
 };
 
@@ -46,6 +130,9 @@ const nextConfig: NextConfig = {
     '@opentelemetry/sdk-node', 
     'handlebars'
   ],
+  serverActions: {
+    bodySizeLimit: '2mb', // A standard configuration to ensure server actions can handle reasonable payloads
+  },
 };
 
 const pwaPlugin = withPWA(pwaConfig);
